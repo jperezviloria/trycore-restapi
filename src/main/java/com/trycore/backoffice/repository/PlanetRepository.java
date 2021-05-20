0e6e7ef630bbce64c478c48313772d4ce60cbf6f
jperezviloria <protobot.dev@gmail.com>
2021-05-20 00:14:32 -0300

    creating getAll and create queries to manipulate data using the repository

--- src/main/java/com/trycore/backoffice/repository/PlanetRepository.java
+++ src/main/java/com/trycore/backoffice/repository/PlanetRepository.java
@@ -1,4 +1,39 @@
 package com.trycore.backoffice.repository;
 
-public interface PlanetRepository {
+import com.trycore.backoffice.model.Planet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
+
+    /*
+    String queryGetPostByIdSubreddit = "SELECT * FROM Post WHERE  idSubreddit = :idSubreddit ";
+    @Query(value = queryGetPostByIdSubreddit, nativeQuery = true)
+    List<Post> getPostByIdSubreddit(
+            @Param("idSubreddit") Long idSubreddit
+    );
+     */
+
+    String queryGetAllPlanets = "SELECT * FROM Planet ;";
+    @Query(value = queryGetAllPlanets, nativeQuery = true)
+    List<Planet> getAllPlanets();
+
+    String queryCreatePlanet = "INSERT INTO Planet (NAME, ROTATION_PERIOD, DIAMETER, WEATHER, LAND, COUNTER) " +
+            "VALUES ( :name , :rotationPeriod , :diameter , :weather , :land , :counter );";
+    @Query(value = queryCreatePlanet, nativeQuery = true)
+    void createPlanet(
+            @Param("name") String name,
+            @Param("rotationPeriod") int rotationPeriod,
+            @Param("diameter") int diameter,
+            @Param("weather") int weather,
+            @Param("land") String land,
+            @Param("counter") int counter
+    );
+
+
 }
