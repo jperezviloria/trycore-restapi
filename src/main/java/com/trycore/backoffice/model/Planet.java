940b6db88ac71e28370b74c921bbf197bdcdccdc
jperezviloria <protobot.dev@gmail.com>
2021-05-19 23:41:31 -0300

    creating planet model

--- src/main/java/com/trycore/backoffice/model/Planet.java
+++ src/main/java/com/trycore/backoffice/model/Planet.java
@@ -1,4 +1,57 @@
 package com.trycore.backoffice.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Planet")
 public class Planet {
+
+    @Id
+    @Column(name = "id")
+    private int idPlanet;
+
+    @Column(name = "name")
+    private String namePlanet;
+
+    @Column(name = "rotation_period")
+    private int rotationPeriodPlanet;
+
+    @Column(name = "diameter")
+    private int diameterPlanet;
+
+    @Column(name = "weather")
+    private int weatherPlanet;
+
+    @Column(name = "land")
+    private int landPlanet;
+
+    @Column(name = "counter")
+    private int counterPlanet;
+
+    public Planet(int idPlanet,
+                  String namePlanet,
+                  int rotationPeriodPlanet,
+                  int diameterPlanet,
+                  int weatherPlanet,
+                  int landPlanet,
+                  int counterPlanet) {
+        this.idPlanet = idPlanet;
+        this.namePlanet = namePlanet;
+        this.rotationPeriodPlanet = rotationPeriodPlanet;
+        this.diameterPlanet = diameterPlanet;
+        this.weatherPlanet = weatherPlanet;
+        this.landPlanet = landPlanet;
+        this.counterPlanet = counterPlanet;
+    }
+
 }
